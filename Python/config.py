53af7bc4574aae786f970baec344a816af2db0ef
HannahSMMcCormick <114184694+HannahSMMcCormick@users.noreply.github.com>
2025-12-05 03:10:12 +0000

    Scraped Posts

---
+++ Python/config.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import os
+
+
+Project_ROOT = Path(__file__).resolve().parents[1]
+
+#Data directories
+DATA_DIR = Project_ROOT / 'Data'
+Cleaned_DATA_DIR = DATA_DIR / 'Cleaned_Data'
+RAW_DATA_DIR = DATA_DIR / 'Raw_Data'
+Results_DIR = DATA_DIR / 'Results'
+
+#Data Output 
+
+OUTPUT_PATH = RAW_DATA_DIR / 'Scraped.csv'
+
+#Env
+Env = Project_ROOT / '.env'
+
+#Might need this not sure yet
+def load_functions(module,path):
+    import importlib.util
+    spec = importlib.util.spec_from_file_location(module, path)
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    return mod
+
+# Function to get a function from a module
+def getfunction(mod, function_name: str):
+    func = getattr(mod, function_name)
+    return func
